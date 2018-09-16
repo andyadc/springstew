dcd185babdb6ed87003f7d0e327af3549087a670
andyadc <andaicheng@gmail.com>
2018-09-16 16:25:30 +0800

    Initial projects

---
+++ springboot-speedup/src/main/java/com/andyadc/springboot/Startup.java
@@ -0,0 +1,15 @@
+package com.andyadc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author andaicheng
+ * @since 2018/9/16
+ */
+@SpringBootApplication
+public class Startup {
+    public static void main(String[] args) {
+        SpringApplication.run(Startup.class);
+    }
+}
