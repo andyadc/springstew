7769e287c0c3141872b893b0ca3f90b0dd361438
99BILL\andy.an <andy.an@99bill.com>
2019-04-09 20:21:30 +0800

    ssm demo sql

--- ssm-demo/doc/demo.sql
+++ ssm-demo/doc/demo.sql
@@ -7,17 +7,18 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS `ssm_demo`;
 CREATE TABLE `ssm_demo` (
-  `id`           BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT
+  `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT
   COMMENT '主键',
-  `name`         VARCHAR(50)         NOT NULL
+  `name`        VARCHAR(50)         NOT NULL
   COMMENT '用户名',
-  `status`       TINYINT(2)          NOT NULL
+  `type`        INT(5) COMMENT '类型',
+  `status`      TINYINT(2)          NOT NULL
   COMMENT '状态',
-  `version`      INT(11)    UNSIGNED          DEFAULT '0'
+  `version`     INT(11)    UNSIGNED          DEFAULT '0'
   COMMENT '版本号',
-  `created_time` DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `create_time` DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP
   COMMENT '创建时间',
-  `updated_time` DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `update_time` DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   COMMENT '更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_name` (`name`) USING BTREE
