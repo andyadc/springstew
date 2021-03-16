018962fa3583e53c527e4e4db0a78d674d9b099d
andaicheng <andaicheng@gmail.com>
2021-03-16 19:56:44 +0800

    security

--- bms/src/main/java/com/andyadc/bms/web/UserController.java
+++ bms/src/main/java/com/andyadc/bms/web/UserController.java
@@ -4,31 +4,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
-    @GetMapping("/api/me")
+    @GetMapping("/me")
     public Object get() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
     @PreAuthorize("hasAuthority('user:add')")
-    @GetMapping("/api/user/add")
-    public Object add() {
-        return ResponseEntity.ok().build();
+    @GetMapping("/user/add")
+    public ResponseEntity<Object> add() {
+        return ResponseEntity.ok("Add user success");
     }
 
     @PreAuthorize("hasAuthority('user:del')")
-    @GetMapping("/api/user/del")
+    @GetMapping("/user/del")
     public ResponseEntity<Object> del() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok("Del user success");
     }
 
     @PreAuthorize("hasAuthority('user:update')")
-    @GetMapping("/api/user/update")
-    public Object update() {
-        return ResponseEntity.ok().build();
+    @GetMapping("/user/update")
+    public ResponseEntity<Object> update() {
+        return ResponseEntity.ok("Update user success");
     }
 }
