9427a1c596193039aeb36d8580bae300c7b141c7
andaicheng <andaicheng@gmail.com>
2021-05-08 16:47:00 +0800

    mybatis

--- spring-boot-data/mybatis/src/main/java/com/andyadc/data/mapper/EmployeeMapper.java
+++ spring-boot-data/mybatis/src/main/java/com/andyadc/data/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.andyadc.data.mapper;
 
 import com.andyadc.data.entity.Employee;
+import org.apache.ibatis.annotations.DeleteProvider;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Result;
@@ -32,13 +33,19 @@ public interface EmployeeMapper {
     @Update("UPDATE employee SET name = #{name} WHERE id = #{id}")
     Integer updateById(Employee employee);
 
-    @SelectProvider(type = UserSqlBuilder.class, method = "buildGetEmployeesByName")
+    @SelectProvider(type = UserSqlProvider.class, method = "buildGetEmployeesByName")
     List<Employee> selectByName(String name);
+
+    @SelectProvider(type = UserSqlProvider.class, method = "buildGetEmployeesByExample")
+    List<Employee> selectByExample(Employee example);
+
+    @DeleteProvider(type = UserSqlProvider.class, method = "buildDeleteByExample")
+    int delByExample(Employee example);
 
     String selectNow();
 
     // implements ProviderMethodResolver
-    class UserSqlBuilder {
+    class UserSqlProvider {
         public static String buildGetEmployeesByName(final String name) {
             return new SQL() {
                 {
@@ -50,6 +57,42 @@ public interface EmployeeMapper {
                     ORDER_BY("id");
                 }
             }.toString();
+        }
+
+        public static String buildGetEmployeesByExample(final Employee example) {
+            SQL sql = new SQL();
+            sql.SELECT("*").FROM("employee");
+            if (example.getName() != null && example.getName().trim().length() > 0) {
+                sql.AND().WHERE("name like concat('%', #{name}, '%')");
+            }
+            // ...
+            sql.ORDER_BY("id desc", "name asc");
+            return sql.toString();
+        }
+
+        public static String buildUpdateEmployeeByExample(final Employee example) {
+            SQL sql = new SQL();
+            sql.UPDATE("employee");
+            if (example.getName() != null && example.getName().trim().length() > 0) {
+                sql.SET("name = #{name}");
+            }
+            sql.WHERE("id = #{id}");
+            return sql.toString();
+        }
+
+        public static String buildSaveEmployee(final Employee example) {
+            SQL sql = new SQL();
+            sql.INSERT_INTO("employee");
+            sql.INTO_COLUMNS("name")
+                    .INTO_VALUES("#{name}");
+            return sql.toString();
+        }
+
+        public static String buildDeleteByExample(final Employee example) {
+            SQL sql = new SQL();
+            sql.DELETE_FROM("employee");
+            sql.WHERE("name=#{name}", "id=#{id}");
+            return sql.toString();
         }
     }
 }
--- spring-boot-data/mybatis/src/test/java/com/andyadc/data/test/MapperTests.java
+++ spring-boot-data/mybatis/src/test/java/com/andyadc/data/test/MapperTests.java
@@ -1,16 +1,28 @@
 package com.andyadc.data.test;
 
+import com.andyadc.data.entity.Employee;
 import com.andyadc.data.mapper.EmployeeMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.annotation.Resource;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class MapperTests {
 
     @Resource
     public EmployeeMapper employeeMapper;
+
+    @Test
+    public void testDel() {
+        Employee example = new Employee();
+        example.setId(1L);
+        example.setName("adc");
+        employeeMapper.delByExample(example);
+    }
 
     @Test
     public void testEmployeeMapper() {
@@ -24,6 +36,9 @@ public class MapperTests {
 
 //        System.out.println(employee.getId());
 
-        employeeMapper.selectByName("adc").forEach(System.out::println);
+//        employeeMapper.selectByName("adc").forEach(System.out::println);
+        Employee example = new Employee();
+        example.setName("adc");
+        employeeMapper.selectByExample(example).forEach(System.out::println);
     }
 }
