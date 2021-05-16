f8f629cf7d8504ff5f6307161ebbecce8fc4aadd
andaicheng <andaicheng@gmail.com>
2021-05-16 18:04:14 +0800

    abatis

---
+++ spring-boot-data/abatis-spring-boot-starter-test/src/main/java/com/andyadc/abatis/spring/boot/test/AbatisApp.java
@@ -0,0 +1,12 @@
+package com.andyadc.abatis.spring.boot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbatisApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbatisApp.class, args);
+    }
+}
--- spring-boot-data/abatis-spring-boot-starter-test/src/test/java/com/andyadc/abatis/spring/boot/test/test/TestTest.java
+++ spring-boot-data/abatis-spring-boot-starter-test/src/test/java/com/andyadc/abatis/spring/boot/test/test/TestTest.java
@@ -1,7 +1,19 @@
 package com.andyadc.abatis.spring.boot.test.test;
 
+import com.alibaba.fastjson.JSON;
+import com.andyadc.abatis.spring.boot.test.User;
+import com.andyadc.abatis.spring.boot.test.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class TestTest {
+
+    private UserMapper userMapper;
+
+    @Test
+    public void testQuery() {
+        User user = userMapper.selectByName("a1");
+        System.out.println(JSON.toJSONString(user));
+    }
 }
